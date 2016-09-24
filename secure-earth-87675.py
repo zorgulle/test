516ced3ae9d2bd08be7a4e31631e228ecc41ab87
Adrien <winy95battle@gmail.com>
2016-09-24 20:52:19 +0200

    Start app dev

--- secure-earth-87675.py
+++ secure-earth-87675.py
@@ -1,14 +1,20 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-def reply(msg=None):
+def process_translation(msg=None):
     return msg
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Index"
+    if request.method == 'POST':
+        msg = request.form['to_translate']
+        translation = process_translation(msg=msg)
+    else:
+        translation = ''
+    return render_template('index.html', translation=translation)
 
 if __name__ == '__main__':
     app.run()
