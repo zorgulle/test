3f80c879002c38d8ecc644af6b57a6f4ae2ef969
Adrien <winy95battle@gmail.com>
2016-09-25 13:11:55 +0200

    Begin unit testing

--- tests.py
+++ tests.py
@@ -1,3 +1,33 @@
+import pytest
+
+from .translator import split
+from .translator import lower_case
+
+class TestLowerCase:
+    """Test case for lowercase
+    """
+
+    def test_none_sentence(self):
+        """Test the none sentence args
+        """
+        with pytest.raises(Exception) as e:
+            sentence = lower_case()
+
+class TestSplit:
+    """Unit test case for split
+
+    split function split sentence into words
+    """
+
+    def test_basic_split(self):
+        """basic sentence no ponctuation no numbers
+        """
+
+        sentence = "i am foo"
+        expected  = ['i', 'am', 'foo']
+
+        result = split(sentence=sentence)
+        assert result == expected
 
 def test_void():
     assert True
---
+++ translator.py
@@ -0,0 +1,12 @@
+"""Contain all the tools to translate
+"""
+
+def lower_case(sentence=None):
+    if sentence is None:
+        raise Exception("None sentence")
+
+def split(sentence=None):
+    """split a sentence in words lower case
+    must detect numbers and ponctiation
+    """
+    raise Exception("Not implemented yes")
